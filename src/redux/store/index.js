6723c1ea4a01afde6b5ad19fa257530008022546
Chukwuemeka Ajima <chukwuemekaajima@Chukwuemekas-MacBook-Pro.local>
2018-12-31 18:11:16 +0100

    - Add dependencies - Add files to hold functionalities for action, reducers and store - Install dependencies
    [Finishes #162894201]

---
+++ src/redux/store/index.js
@@ -0,0 +1,25 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-extraneous-dependencies */
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from '../reducers/rootReducer';
+
+const middleware = process.env.NODE_ENV !== 'production'
+  ? [require('redux-immutable-state-invariant').default(), thunk]
+  : [thunk];
+
+/**
+ * @param {*} initialState
+ * @return {*} createStore
+ */
+function configureStore() {
+  return createStore(
+    rootReducer,
+    applyMiddleware(...middleware),
+  );
+}
+
+const store = configureStore();
+window.store = store;
+
+export default store;
